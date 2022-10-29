ca67620cb6f907ad3b8e41e04ecf7a4d021cdb44
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-10-29 17:14:35 +0900

    feat(member): service dto

---
+++ src/main/java/com/lhgpds/algometa/internal/member/service/dto/MemberDto.java
@@ -0,0 +1,25 @@
+package com.lhgpds.algometa.internal.member.service.dto;
+
+import com.lhgpds.algometa.internal.member.entity.vo.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberDto {
+
+    private Long id;
+    private String email;
+    private String nickname;
+    private String image;
+    private Role role;
+
+}
