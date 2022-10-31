7d3efab812954606d05dddb4fde15f008ef981ab
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-10-31 10:14:33 +0900

    feat(member): upload mapper 추가

--- src/main/java/com/lhgpds/algometa/mapper/MemberMapper.java
+++ src/main/java/com/lhgpds/algometa/mapper/MemberMapper.java
@@ -4,6 +4,7 @@ import com.lhgpds.algometa.controller.auth.dto.TokenDto;
 import com.lhgpds.algometa.controller.common.dto.ResponseCommon;
 import com.lhgpds.algometa.controller.member.dto.RequestUpdateProfile;
 import com.lhgpds.algometa.controller.member.dto.ResponseProfile;
+import com.lhgpds.algometa.controller.member.dto.ResponseUploadImage;
 import com.lhgpds.algometa.internal.member.entity.Member;
 import com.lhgpds.algometa.internal.member.service.dto.MemberDto;
 import org.mapstruct.Mapper;
@@ -27,4 +28,6 @@ public interface MemberMapper {
     ResponseProfile convertToResponseUpdateProfile(MemberDto memberDto);
 
     ResponseCommon convertToResponseCommon(MemberDto memberDto);
+
+    ResponseUploadImage convertToResponseUploadImage(MemberDto memberDto);
 }
