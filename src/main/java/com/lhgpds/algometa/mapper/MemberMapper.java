170253b144d72e9ed64f74f14ae95bbe387ac30d
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-25 18:49:59 +0900

    feat(member): 개인 회원 출력 response dto 변환 메서드 추가

--- src/main/java/com/lhgpds/algometa/mapper/MemberMapper.java
+++ src/main/java/com/lhgpds/algometa/mapper/MemberMapper.java
@@ -3,6 +3,7 @@ package com.lhgpds.algometa.mapper;
 import com.lhgpds.algometa.controller.auth.dto.TokenDto;
 import com.lhgpds.algometa.controller.common.dto.ResponseCommon;
 import com.lhgpds.algometa.controller.member.dto.RequestUpdateProfile;
+import com.lhgpds.algometa.controller.member.dto.ResponseMyProfile;
 import com.lhgpds.algometa.controller.member.dto.ResponseProfile;
 import com.lhgpds.algometa.controller.member.dto.ResponseUploadImage;
 import com.lhgpds.algometa.internal.member.entity.Member;
@@ -30,4 +31,6 @@ public interface MemberMapper {
     ResponseCommon convertToResponseCommon(MemberDto memberDto);
 
     ResponseUploadImage convertToResponseUploadImage(MemberDto memberDto);
+
+    ResponseMyProfile convertToResponseMyProfile(MemberDto memberDto);
 }
