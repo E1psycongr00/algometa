71497770c894ab6fb7191a186f7b052d7d8c8c0f
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-16 09:29:56 +0900

    feat(mapper): 원시값과 VO 사이의 변환 로직 추가

--- src/main/java/com/lhgpds/algometa/mapper/MemberMapper.java
+++ src/main/java/com/lhgpds/algometa/mapper/MemberMapper.java
@@ -1,18 +1,21 @@
 package com.lhgpds.algometa.mapper;
 
-import com.lhgpds.algometa.controller.auth.dto.TokenDto;
 import com.lhgpds.algometa.controller.common.dto.ResponseCommon;
 import com.lhgpds.algometa.controller.member.dto.RequestUpdateProfile;
 import com.lhgpds.algometa.controller.member.dto.ResponseMyProfile;
 import com.lhgpds.algometa.controller.member.dto.ResponseProfile;
 import com.lhgpds.algometa.controller.member.dto.ResponseUploadImage;
 import com.lhgpds.algometa.internal.member.domain.entity.Member;
+import com.lhgpds.algometa.internal.member.domain.vo.Email;
+import com.lhgpds.algometa.internal.member.domain.vo.ImageLink;
+import com.lhgpds.algometa.internal.member.domain.vo.Nickname;
 import com.lhgpds.algometa.internal.member.service.dto.MemberDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface MemberMapper {
 
     MemberMapper instance = Mappers.getMapper(MemberMapper.class);
@@ -30,4 +33,28 @@ public interface MemberMapper {
     ResponseUploadImage convertToResponseUploadImage(MemberDto memberDto);
 
     ResponseMyProfile convertToResponseMyProfile(MemberDto memberDto);
+
+    default String fromEmail(Email email) {
+        return email.toString();
+    }
+
+    default Email toEmail(String email) {
+        return Email.from(email);
+    }
+
+    default String fromNickname(Nickname nickname) {
+        return nickname == null ? null : nickname.toString();
+    }
+
+    default Nickname toNickname(String nickname) {
+        return nickname == null ? null : Nickname.from(nickname);
+    }
+
+    default String fromImageLink(ImageLink imageLink) {
+        return imageLink == null ? null : imageLink.toString();
+    }
+
+    default ImageLink toImageLink(String imageLink) {
+        return imageLink == null ? null : ImageLink.from(imageLink);
+    }
 }
