9af064cc1c8c59a5021e51f733601a6c12a05787
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-20 13:27:56 +0900

    feat(problem): problemService 추가

---
+++ src/main/java/com/lhgpds/algometa/internal/problem/service/ProblemService.java
@@ -0,0 +1,30 @@
+package com.lhgpds.algometa.internal.problem.service;
+
+import com.lhgpds.algometa.internal.member.service.dto.MemberDto;
+import com.lhgpds.algometa.internal.problem.domain.entity.History;
+import com.lhgpds.algometa.internal.problem.domain.entity.Problem;
+import com.lhgpds.algometa.internal.problem.repository.HistoryRepository;
+import com.lhgpds.algometa.internal.problem.repository.ProblemRepository;
+import com.lhgpds.algometa.internal.problem.service.dto.ProblemDto;
+import com.lhgpds.algometa.mapper.ProblemMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ProblemService {
+
+    private final ProblemRepository problemRepository;
+    private final HistoryRepository historyRepository;
+
+    @Transactional
+    public ProblemDto addProblem(MemberDto userDto, ProblemDto problemDto) {
+        Problem problem = ProblemMapper.instance.toEntity(problemDto);
+        problem.setMemberId(userDto.getId());
+        History history = problem.snapShotCode();
+        historyRepository.save(history);
+        Problem resultProblem = problemRepository.save(problem);
+        return ProblemMapper.instance.toDto(resultProblem);
+    }
+}
