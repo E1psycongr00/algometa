bf7a6ec6083fab1c905dbcadf343788c75b8b553
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-20 13:28:26 +0900

    feat(problem): problemDto 추가

---
+++ src/main/java/com/lhgpds/algometa/internal/problem/service/dto/ProblemDto.java
@@ -0,0 +1,32 @@
+package com.lhgpds.algometa.internal.problem.service.dto;
+
+import com.lhgpds.algometa.internal.problem.domain.vo.Platform;
+import com.lhgpds.algometa.internal.problem.domain.vo.ProblemId;
+import com.lhgpds.algometa.internal.problem.domain.vo.code.Category;
+import com.lhgpds.algometa.internal.problem.domain.vo.code.Code;
+import com.lhgpds.algometa.internal.problem.domain.vo.content.Content;
+import java.time.LocalDateTime;
+import java.util.Set;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@ToString
+public class ProblemDto {
+
+    private ProblemId problemId;
+    private Content content;
+    private Code code;
+    private Set<Category> category;
+    private Platform platform;
+    private Long memberId;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+}
