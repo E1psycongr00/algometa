3ffa5def165f72fabbe2d98dd8cfbe48420d12c4
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-20 13:25:53 +0900

    feat(mapper): problem mapper 추가

---
+++ src/main/java/com/lhgpds/algometa/mapper/ProblemMapper.java
@@ -0,0 +1,24 @@
+package com.lhgpds.algometa.mapper;
+
+import com.lhgpds.algometa.controller.problem.dto.RequestAddProblem;
+import com.lhgpds.algometa.controller.problem.dto.ResponseProblemId;
+import com.lhgpds.algometa.internal.problem.domain.entity.Problem;
+import com.lhgpds.algometa.internal.problem.service.dto.ProblemDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface ProblemMapper {
+
+    ProblemMapper instance = Mappers.getMapper(ProblemMapper.class);
+
+
+    Problem toEntity(ProblemDto problemDto);
+
+    ProblemDto toDto(Problem problem);
+
+    ProblemDto toDto(RequestAddProblem requestAddProblem);
+
+    ResponseProblemId toResponseProblemId(ProblemDto problemDto);
+}
