d7b70d3ce967f9c5ad9ce74ded00cf0b75f93434
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-20 14:41:45 +0900

    feat(problem): 서비스에 updateProblem 기능 추가

--- src/main/java/com/lhgpds/algometa/internal/problem/application/ProblemService.java
+++ src/main/java/com/lhgpds/algometa/internal/problem/application/ProblemService.java
@@ -1,11 +1,15 @@
 package com.lhgpds.algometa.internal.problem.application;
 
+import com.lhgpds.algometa.exception.common.problem.ProblemNotFoundException;
 import com.lhgpds.algometa.internal.member.application.dto.MemberDto;
+import com.lhgpds.algometa.internal.problem.application.dto.ProblemDto;
+import com.lhgpds.algometa.internal.problem.application.utils.ProblemAuthorityChecker;
 import com.lhgpds.algometa.internal.problem.domain.entity.History;
 import com.lhgpds.algometa.internal.problem.domain.entity.Problem;
+import com.lhgpds.algometa.internal.problem.domain.vo.ProblemId;
+import com.lhgpds.algometa.internal.problem.domain.vo.content.Content;
 import com.lhgpds.algometa.internal.problem.repository.HistoryRepository;
 import com.lhgpds.algometa.internal.problem.repository.ProblemRepository;
-import com.lhgpds.algometa.internal.problem.application.dto.ProblemDto;
 import com.lhgpds.algometa.mapper.ProblemMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -27,4 +31,14 @@ public class ProblemService {
         Problem resultProblem = problemRepository.save(problem);
         return ProblemMapper.instance.toDto(resultProblem);
     }
+
+    @Transactional
+    public ProblemDto updateProblemContent(long memberId, ProblemId problemId, Content content) {
+        Problem problem = problemRepository.findById(problemId)
+            .orElseThrow(ProblemNotFoundException::new);
+        ProblemAuthorityChecker.check(memberId, problem);
+        problem.changeContent(content);
+        return ProblemMapper.instance.toDto(problem);
+    }
+
 }
