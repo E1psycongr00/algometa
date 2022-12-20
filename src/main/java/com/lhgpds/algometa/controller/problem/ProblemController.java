18b12d9b9a03783f9bb31aed3b710aff1e477532
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-20 16:05:59 +0900

    feat(problem): 컨트롤러에 문제 컨텐츠 등록 api 추가

--- src/main/java/com/lhgpds/algometa/controller/problem/ProblemController.java
+++ src/main/java/com/lhgpds/algometa/controller/problem/ProblemController.java
@@ -1,11 +1,13 @@
 package com.lhgpds.algometa.controller.problem;
 
 import com.lhgpds.algometa.controller.problem.dto.RequestAddProblem;
+import com.lhgpds.algometa.controller.problem.dto.RequestUpdateProblemContent;
 import com.lhgpds.algometa.controller.problem.dto.ResponseProblemId;
 import com.lhgpds.algometa.internal.auth.jwt.principal.AlgoUser;
 import com.lhgpds.algometa.internal.member.application.dto.MemberDto;
 import com.lhgpds.algometa.internal.problem.application.ProblemService;
 import com.lhgpds.algometa.internal.problem.application.dto.ProblemDto;
+import com.lhgpds.algometa.internal.problem.domain.vo.ProblemId;
 import com.lhgpds.algometa.mapper.ProblemMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,7 +16,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +41,19 @@ public class ProblemController {
         ProblemDto responseDto = problemService.addProblem(userDto, requestDto);
         ResponseProblemId response = ProblemMapper.instance.toResponseProblemId(responseDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @Secured("ROLE_USER")
+    @PutMapping("/{id}/contents")
+    public ResponseEntity<ResponseProblemId> updateProblemContent(
+        @AuthenticationPrincipal AlgoUser algoUser,
+        @PathVariable(value = "id") ProblemId problemId,
+        @RequestBody @Validated RequestUpdateProblemContent requestUpdateProblemContent) {
+
+        MemberDto memberDto = algoUser.getMemberDto();
+        ProblemDto problemDto = problemService.updateProblemContent(memberDto.getId(), problemId,
+            requestUpdateProblemContent.getContent());
+        ResponseProblemId responseProblemId = ProblemMapper.instance.toResponseProblemId(problemDto);
+        return ResponseEntity.status(HttpStatus.OK).body(responseProblemId);
     }
 }
