87a5ce0868a19b7e261e196b550feb329c47331b
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-22 13:58:34 +0900

    feat(problem): 히스토리 레포지토리 추가

--- src/main/java/com/lhgpds/algometa/internal/problem/repository/HistoryRepository.java
+++ src/main/java/com/lhgpds/algometa/internal/problem/repository/HistoryRepository.java
@@ -1,9 +1,15 @@
 package com.lhgpds.algometa.internal.problem.repository;
 
+import com.lhgpds.algometa.infrastructure.querydsl.repository.QueryDslHistoryRepository;
 import com.lhgpds.algometa.internal.problem.domain.entity.History;
 import com.lhgpds.algometa.internal.problem.domain.vo.HistoryId;
+import com.lhgpds.algometa.internal.problem.domain.vo.ProblemId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HistoryRepository extends JpaRepository<History, HistoryId> {
+public interface HistoryRepository extends JpaRepository<History, HistoryId>,
+    QueryDslHistoryRepository {
+
+    long countByProblemId(ProblemId problemId);
+
 
 }
