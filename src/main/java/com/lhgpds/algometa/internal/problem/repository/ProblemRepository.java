e398eaaed751f497b0c6a9aac076bfb18dc2b4df
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-23 15:29:23 +0900

    feat(problem): querydsl 인터페이스 상속

--- src/main/java/com/lhgpds/algometa/internal/problem/repository/ProblemRepository.java
+++ src/main/java/com/lhgpds/algometa/internal/problem/repository/ProblemRepository.java
@@ -1,9 +1,12 @@
 package com.lhgpds.algometa.internal.problem.repository;
 
+import com.lhgpds.algometa.infrastructure.querydsl.repository.QueryDslProblemRepository;
 import com.lhgpds.algometa.internal.problem.domain.entity.Problem;
 import com.lhgpds.algometa.internal.problem.domain.vo.ProblemId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProblemRepository extends JpaRepository<Problem, ProblemId> {
+public interface ProblemRepository extends JpaRepository<Problem, ProblemId>,
+    QueryDslProblemRepository {
+
 
 }
